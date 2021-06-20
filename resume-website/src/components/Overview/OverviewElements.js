c8748a35b2d0ba12370de7dd4e719aa81f0ee705
TylerLott <tylerlott0126@gmail.com>
2021-06-20 11:33:34 -0600

    autoplay

--- resume-website/src/components/Overview/OverviewElements.js
+++ resume-website/src/components/Overview/OverviewElements.js
@@ -33,7 +33,7 @@ export const OverviewBg = styled.div`
   overflow: hidden;
 `
 
-export const VideoBg = styled.video`
+export const VideoBg = styled.div`
   width: 100%;
   height: 100%;
   -o-object-fit: cover;
